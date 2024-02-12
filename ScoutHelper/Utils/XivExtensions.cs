784033babcfc73c6fa1df7afb0226d922fca2dbf
ditzy <ditzy.zy@pm.me>
2024-02-12 02:45:36 -0600

    support hard coded instances for siren

--- ScoutHelper/Utils/XivExtensions.cs
+++ ScoutHelper/Utils/XivExtensions.cs
@@ -10,7 +10,7 @@ using ScoutHelper.Models;
 namespace ScoutHelper.Utils;
 
 public static partial class XivExtensions {
-	[GeneratedRegex(@" \d{1,2}$")] private static partial Regex UnInstanceRegex();
+	[GeneratedRegex(@" \d{1,2}$")] private static partial Regex InstanceRegex();
 
 	public static string WorldName(this IClientState clientState) =>
 		clientState.LocalPlayer?.CurrentWorld.GameData?.Name.ToString() ?? "Not Found";
@@ -28,8 +28,15 @@ public static partial class XivExtensions {
 		chatGui.PrintError(message, Plugin.Name);
 	}
 
-	public static Maybe<TrainMob> FindMob(this IEnumerable<TrainMob> mobList, uint mobId) =>
-		Maybe.From(mobList.FirstOrDefault(mob => mob.MobId == mobId));
+	public static Maybe<TrainMob> FindMob(this IEnumerable<TrainMob> mobList, uint mobId, uint? instance = null) =>
+		Maybe.From(mobList.FirstOrDefault(mob => mob.MobId == mobId && mob.Instance == instance));
 
-	public static string UnInstanced(this string mobName) => UnInstanceRegex().Replace(mobName, "");
+	public static string UnInstanced(this string mobName) => InstanceRegex().Replace(mobName, "");
+	
+	public static uint? Instance(this string mobName) => InstanceRegex()
+		.Match(mobName)
+		.AsMaybe()
+		.Where(match => match.Success)
+		.Map(match => uint.Parse(match.Value))
+		.GetValueOrDefault();
 }
